88a6770ab264cb2fdc5da01a4b210cdc8ba5820c
marc <marc.mentha@gmail.com>
2018-01-28 18:28:11 +0000

    added lua funtion which reads bash script and extracts the function to a single file each

---
+++ lua/funtionToFile.lua
@@ -0,0 +1,35 @@
+local args = {...}
+local filename = "";
+
+for key, arg in pairs(args) do
+    if key == 1 then
+        filename = arg;
+    end
+end
+
+if filename == "" then
+    print("no filename")
+    return
+end
+
+file = io.open(filename, 'r');
+
+functionFound = false;
+os.execute('mkdir luaOutput');
+
+for line in file:lines() do
+    if line:find(' {') then
+        functionFound = true;
+        exportFile = io.open('luaOutput/' .. line:sub( 1, #line - 4 ), 'w');
+    end
+
+    if functionFound then
+        exportFile:write(line);
+    end
+
+    if line:find('}$') then
+        functionFound = false;
+    end
+end
+
+file:close()
