442a93732343cfb139a2172e9fc5f53ee9e3ada2
Marc Mentha <marc.mentha@gmail.com>
2018-02-05 13:32:11 +0100

    updated get repeater fields from acf

--- php/wordPress/acf/get-repeater-field-values.php
+++ php/wordPress/acf/get-repeater-field-values.php
@@ -4,8 +4,11 @@
 if (have_rows('repeater_field_name')) :
 	 // loop through the rows of data
 	while (have_rows('repeater_field_name')) :
+		//unformatted
 		the_row();
 
+		//get the subfields in an array as key value where the key is the fiedl label instad of the field id
+		$row = the_row(true);
 		// display a sub field value
 		the_sub_field('sub_field_name');
 	endwhile;
