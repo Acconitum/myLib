283804fe8a71689c9daaf5ed37bab738e31cd80d
Marc Mentha <marc@kernelpanic.localdomain>
2018-05-08 13:31:04 +0200

    renamed function and deleted unwanted lines

--- javaScript/extract_youtube_id.js
+++ javaScript/extract_youtube_id.js
@@ -1,23 +1,11 @@
-function YouTubeGetID(url){
-
+function getYoutubeId(url){
   var ID = '';
-
   url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-
   if(url[2] !== undefined) {
-
     ID = url[2].split(/[^0-9a-z_\-]/i);
-
     ID = ID[0];
-
-  }
-
-  else {
-
+  } else {
     ID = url;
-
   }
-
     return ID;
-
 }
