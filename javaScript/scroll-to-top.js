e493802272980a3dd3d612cefe334a55546d70ac
Marc Mentha <marc@kernelpanic.localdomain>
2018-06-06 14:42:16 +0200

    lttle change

--- javaScript/scroll-to-top.js
+++ javaScript/scroll-to-top.js
@@ -1,6 +1,6 @@
 (function ($) {
 	$(document).ready(function () {
-		$("[data-back-to-top]").click(function () {
+		$("[data-back-to-top]").on('click', function () {
 			//1 second of animation time
 			$("html").animate({ scrollTop: 0 }, 1000);
 		});
